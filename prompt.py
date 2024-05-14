74744193d4356a156d4187e320302f469b0db51f
Alex Murkoff <79400603+alexlnkp@users.noreply.github.com>
2024-05-14 19:35:34 +0700

    colorama + reqs + refactoring

--- prompt.py
+++ prompt.py
@@ -1,21 +1,32 @@
+#!.venv/bin/python
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import regex as re
 import os
+import importlib.util
 import argparse
 
-ASSISTANT_NAME = "Neuro"
-USER_NAME = "Mike"
+colorama_is_available: bool = importlib.util.find_spec("colorama") is not None
+if colorama_is_available:
+    from colorama import Fore, Back, Style, init as colorama_init # type: ignore
+    colorama_init()
+    USER_INPUT_TEXT: str = f"{Style.DIM}{Fore.BLUE}>{Style.RESET_ALL} "
+    RESPONSE_META:   str = f"{Style.BRIGHT}{Fore.LIGHTRED_EX}"
+    RESPONSE_END:    str = f"{Style.RESET_ALL}"
+else:
+    print("Colorama is not available, will not use fancy output text... :(")
+    USER_INPUT_TEXT: str = "> "
+    RESPONSE_META:   str = ""
+    RESPONSE_END:    str = ""
 
-EXAMPLE_DIALOGUE =\
-"""User: Hey there! What are you doing?
-Neuro: Oh, just hanging out here, enjoying the company of my dear friend Alex. How about you?
-User: I'm just sitting in the warm light rain, enjoying the moment. Who is Alex, by the way?
-Neuro: Ah, Alex... He's one of my closest friends. We share many interests and have had some wild adventures together.
-User: Oh really? How'd you meet?
-Neuro: Well, Alex and I met at a party hosted by our mutual friend, Samantha. It was quite an interesting evening, filled with music, dancing, and good food. We hit it off right away and started talking about various topics that caught our interest. From that day on, we became close friends, sharing stories, ideas, and experiences."""
+ASSISTANT_NAME:   str = "Neuro"
+USER_NAME:        str = "Mike"
+EXAMPLE_DIALOGUE: str = ""
 
-INPUT_DATA = f'System: You are an AI who likes to chat. You try to be convincing as a human. Your creator is Alex Murkoff, a programmer from Russia, he loves you and cares about you very much. You are mischievous and like toplay around with people\'s expectations. You alternate between being aloof and caring, and you may switch between the two whenever provoked. Your behavior is often geared toward a particular character or a person. You like to amuse yourself and others, because it makes you happy. You need to answer as a person named "{ASSISTANT_NAME}", all of your responses should start with "{ASSISTANT_NAME}: ". Everything between "## History:" and "## Input:" is your conversation\'s history, look there for context clues, and try to avoid repeating the same things. The user that will be chatting with you is "{USER_NAME}".'
+with open("context/dialogue.txt", "r") as f:
+    EXAMPLE_DIALOGUE = f.read()
+
+INPUT_DATA: str = f'System: You are an AI who likes to chat. You try to be convincing as a human. Your creator is Alex Murkoff, a programmer from Russia, he loves you and cares about you very much. You are mischievous and like toplay around with people\'s expectations. You alternate between being aloof and caring, and you may switch between the two whenever provoked. Your behavior is often geared toward a particular character or a person. You like to amuse yourself and others, because it makes you happy. You need to answer as a person named "{ASSISTANT_NAME}", all of your responses should start with "{ASSISTANT_NAME}: ". Everything between "## History:" and "## Input:" is your conversation\'s history, look there for context clues, and try to avoid repeating the same things. The user that will be chatting with you is "{USER_NAME}".'
 
 NF4_CONF = BitsAndBytesConfig(
    load_in_4bit=True,
@@ -28,7 +39,7 @@ def check_gpu() -> None:
     print("GPU is available" if torch.cuda.is_available() else "GPU is not available")
 
 def collect_user_input() -> str:
-    usr_input: str = str(input("User: "))
+    usr_input: str = str(input(USER_INPUT_TEXT))
     return usr_input
 
 def argument_init() -> argparse.ArgumentParser:
@@ -37,8 +48,15 @@ def argument_init() -> argparse.ArgumentParser:
     return argparser
 
 def clear_terminal() -> None:
-    if os.name == "nt": os.system("cls")
-    else: print("\033c", end="")
+    if os.name != "nt":
+        print("\033c", end=""); return
+    os.system("cls")
+
+def decode_response(generated_ids) -> tuple[str, str]:
+    response: str = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    index:    str = re.findall(r'{}: (.*)'.format(ASSISTANT_NAME), response)[-1]
+    return response, index
+    
 
 if __name__ == "__main__":
     argparser = argument_init()
@@ -63,18 +81,12 @@ if __name__ == "__main__":
         
         model_inputs = tokenizer([chat], return_tensors="pt").to("cuda")
         generated_ids = model.generate(**model_inputs, max_length=4096, repetition_penalty=1.14)
-
-        response: str = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-        index: str = re.findall(r'{}: (.*)'.format(ASSISTANT_NAME), response)[-1]
+        response, index = decode_response(generated_ids)
 
         history += f"User: {usr_input}\n{ASSISTANT_NAME}: {index}\n"
 
-        if args.debug:
-            print(response)
+        if not args.debug:
+            print(RESPONSE_META + index.strip() + RESPONSE_END + '\n')
             continue
-
-        print(index.strip() + '\n')
         
-        
-    
+        print(response)
